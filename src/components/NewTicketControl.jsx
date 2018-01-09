0e14e8c2487f97960da8a62fb7d905c99db2d615
Luciano Oliveira <luciano101@msn.com>
2018-01-09 12:33:00 -0800

    add NewTicketForm and display on NewTicketControl

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import NewTicketForm from './NewTicketForm';
 
 class NewTicketControl extends Component {
+
   constructor(props){
     super(props);
     this.state = {
@@ -10,18 +12,18 @@ class NewTicketControl extends Component {
   }
 
   handleDisplayingNewTicketForm(event){
-    console.log('button has been clicked:', this);
     this.setState({ formVisibleOnPage: true });
   }
 
   render(){
     let formAreaContent = null;
-    if(this.state.formVisibleOnpage){
-      formAreaContent = <h1>Form will eventuall go here!</h1>
+    if (this.state.formVisibleOnPage){
+      formAreaContent = <NewTicketForm />
     } else {
       formAreaContent = <button onClick={this.handleDisplayingNewTicketForm}>Request for Help</button>
     }
-    return(
+
+    return (
       <div>
         {formAreaContent}
       </div>
---
+++ src/components/NewTicketForm.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default props => {
+  return(
+    <div>
+      <form>
+        <input
+          type="text"
+          id="names"
+          placeholder="Pair Names"/>
+        <input
+          type="text"
+          id="location"
+          placeholder="Location"/>
+        <textarea
+          id="issue"
+          placeholder="Describe your issue."/>
+        <button type="submit">Help!</button>
+      </form>
+    </div>
+  );
+}
+
