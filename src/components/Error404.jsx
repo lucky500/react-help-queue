ab2f9c506c4e46b43ad5ab65e67e8dd0421b5874
Luciano Oliveira <luciano101@msn.com>
2018-01-12 20:27:51 -0800

    add error page

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export default props => {
+export default function Error404(props) {
+  console.log(props);
   return (
     <div>
-      <h3>The page you are looking for does not exist!</h3>
+      <h3>The page {props.location.pathname} does not exist!</h3>
       <h4>Would you like to return <Link to="/">Home</Link> instead?</h4>
     </div>
   )
 }
+
+Error404.propTypes = {
+  location: PropTypes.object
+};
